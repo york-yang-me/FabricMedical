d4fac3106f5a3186e5bbd872540390aa7953b084
月小泪 <york.yang.me@gmail.com>
2024-12-20 15:24:42 +0900

    Add explorer user node configuration

--- network/explorer/start.sh
+++ network/explorer/start.sh
@@ -2,11 +2,13 @@
 
 hospital_priv_sk_path=$(ls ../crypto-config/peerOrganizations/hospital.com/users/Admin\@hospital.com/msp/keystore/)
 patient_priv_sk_path=$(ls ../crypto-config/peerOrganizations/patient.com/users/Admin\@patient.com/msp/keystore/)
+citizen_priv_sk_path=$(ls ../crypto-config/peerOrganizations/citizen.com/users/Admin\@citizen.com/msp/keystore/)
 
 # cp -rf ./connection-profile/network_temp.json ./connection-profile/hospital_network.json
 
 sed -i "s/priv_sk/$hospital_priv_sk_path/" ./connection-profile/hospital_network.json
 sed -i "s/priv_sk/$patient_priv_sk_path/" ./connection-profile/patient_network.json
+sed -i "s/priv_sk/$citizen_priv_sk_path/" ./connection-profile/citizen_network.json
 
 docker-compose down -v
 docker-compose up -d
